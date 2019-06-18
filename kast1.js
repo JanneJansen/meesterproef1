0d3652b132a097defeb23e5d8381ae5f8e2ee0db
JanneJansen <jansenjanne@gmail.com>
2019-06-19 00:40:04 +0200

    openkast gelinkt

---
+++ kast1.js
@@ -0,0 +1,12 @@
+var openen = document.querySelector('.kast1');
+
+
+
+
+
+
+
+openen.addEventListener('click', function(){
+                     console.log('kast');
+    location.href = "kastopen.html";
+});
